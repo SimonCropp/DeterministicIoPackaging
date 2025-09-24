366b71f64dbaaff0e0134c667bc614728679f45a
Simon Cropp <simon.cropp@gmail.com>
2025-09-25 09:33:29 +1000

    scrub creator

--- src/DeterministicIoPackaging/Patching/CorePatcher.cs
+++ src/DeterministicIoPackaging/Patching/CorePatcher.cs
@@ -1,6 +1,8 @@
 ﻿class CorePatcher :
     IPatcher
 {
+    static XNamespace dc = "http://purl.org/dc/elements/1.1/";
+    static XName creator = dc + "creator";
     static XNamespace cp = "http://schemas.openxmlformats.org/package/2006/metadata/core-properties";
     static XName lastModifiedBy = cp + "lastModifiedBy";
     static XNamespace dcterms = "http://purl.org/dc/terms/";
@@ -10,6 +12,7 @@
     public void PatchXml(XDocument xml)
     {
         var root = xml.Root!;
+        root.Element(creator)?.Remove();
         root.Element(lastModifiedBy)?.Remove();
         root.Element(created)?.Remove();
         root.Element(modified)?.Remove();
