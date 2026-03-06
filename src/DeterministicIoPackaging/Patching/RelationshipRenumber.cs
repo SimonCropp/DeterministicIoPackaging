843e6ab5f31691dfc9e54809cd434561facbc240
Simon Cropp <simon.cropp@gmail.com>
2026-03-06 11:03:58 +1100

    .

---
+++ src/DeterministicIoPackaging/Patching/RelationshipRenumber.cs
@@ -0,0 +1,37 @@
+static class RelationshipRenumber
+{
+    public static Dictionary<string, string> RenumberAndSort(XDocument xml)
+    {
+        var root = xml.Root!;
+        var relationships = root.Elements()
+            .OrderBy(_ => _.Attribute("Type")!.Value)
+            .ThenBy(_ => _.Attribute("Target")!.Value)
+            .ToList();
+
+        var mapping = new Dictionary<string, string>();
+        for (var index = 0; index < relationships.Count; index++)
+        {
+            var relationship = relationships[index];
+            var idAttr = relationship.Attribute("Id")!;
+            var newId = $"DeterministicId{index + 1}";
+            mapping[idAttr.Value] = newId;
+            idAttr.SetValue(newId);
+        }
+
+        root.ReplaceAll(relationships);
+        return mapping;
+    }
+
+    public static void RemapIds(XDocument xml, Dictionary<string, string> mapping)
+    {
+        foreach (var attr in xml.Descendants().Attributes())
+        {
+            if (attr.Name.LocalName == "id" &&
+                attr.Name.Namespace != XNamespace.None &&
+                mapping.TryGetValue(attr.Value, out var newId))
+            {
+                attr.SetValue(newId);
+            }
+        }
+    }
+}
