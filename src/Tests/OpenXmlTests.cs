7ca156695295320614734cd69e8755e3166b12b5
Simon Cropp <simon.cropp@gmail.com>
2026-03-19 14:52:07 +1100

    OpenXml compat

---
+++ src/Tests/OpenXmlTests.cs
@@ -0,0 +1,60 @@
+#if NET10_0_OR_GREATER
+
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+[TestFixture]
+public class OpenXmlTests
+{
+    [Test]
+    public Task CreateAndConvert()
+    {
+        using var backingStream = new MemoryStream();
+        using var document = SpreadsheetDocument.Create(backingStream, SpreadsheetDocumentType.Workbook);
+
+        var workbookPart = document.AddWorkbookPart();
+        workbookPart.Workbook = new(new Sheets());
+
+        var worksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+        worksheetPart.Worksheet = new(new SheetData());
+
+        var sheets = workbookPart.Workbook.GetFirstChild<Sheets>()!;
+        sheets.Append(new Sheet
+        {
+            Id = workbookPart.GetIdOfPart(worksheetPart),
+            SheetId = 1,
+            Name = "Sheet1"
+        });
+
+        var sheetData = worksheetPart.Worksheet.GetFirstChild<SheetData>()!;
+
+        var headerRow = new Row { RowIndex = 1 };
+        headerRow.Append(CreateCell("A1", "Name"));
+        headerRow.Append(CreateCell("B1", "Value"));
+        sheetData.Append(headerRow);
+
+        var dataRow = new Row { RowIndex = 2 };
+        dataRow.Append(CreateCell("A2", "Test"));
+        dataRow.Append(CreateCell("B2", "123"));
+        sheetData.Append(dataRow);
+
+        document.Clone(backingStream);
+
+        backingStream.Position = 0;
+        var result = DeterministicPackage.Convert(backingStream);
+
+        return Verify(result, extension: "xlsx")
+            .UniqueForRuntime();
+    }
+
+    static Cell CreateCell(string reference, string value) =>
+        new()
+        {
+            CellReference = reference,
+            DataType = CellValues.InlineString,
+            InlineString = new(new Text(value))
+        };
+}
+
+#endif
