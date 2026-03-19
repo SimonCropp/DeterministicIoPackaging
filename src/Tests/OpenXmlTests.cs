b978fe791ff6cc23d18d8af5c482f574f2ae04ca
Simon Cropp <simon.cropp@gmail.com>
2026-03-19 16:14:08 +1100

    Update OpenXmlTests.cs

--- src/Tests/OpenXmlTests.cs
+++ src/Tests/OpenXmlTests.cs
@@ -16,48 +16,47 @@ public class OpenXmlTests
 
     static MemoryStream CreateSpreadsheet()
     {
-        var backingStream = new MemoryStream();
-        using var document = SpreadsheetDocument.Create(backingStream, SpreadsheetDocumentType.Workbook);
-
-        var workbookPart = document.AddWorkbookPart();
-        workbookPart.Workbook = new(new Sheets());
-
-        var worksheetPart = workbookPart.AddNewPart<WorksheetPart>();
-        worksheetPart.Worksheet = new(new SheetData());
-
-        var sheets = workbookPart.Workbook.GetFirstChild<Sheets>()!;
-        sheets.Append(new Sheet
+        var stream = new MemoryStream();
+        using (var document = SpreadsheetDocument.Create(stream, SpreadsheetDocumentType.Workbook))
         {
-            Id = workbookPart.GetIdOfPart(worksheetPart),
-            SheetId = 1,
-            Name = "Sheet1"
-        });
+            var workbookPart = document.AddWorkbookPart();
+            workbookPart.Workbook = new(new Sheets());
 
-        var stylesPart = workbookPart.AddNewPart<WorkbookStylesPart>();
-        stylesPart.Stylesheet = new(
-            new Fonts(new Font()),
-            new Fills(
-                new Fill(new PatternFill { PatternType = PatternValues.None }),
-                new Fill(new PatternFill { PatternType = PatternValues.Gray125 })),
-            new Borders(new Border()),
-            new CellFormats(new CellFormat()));
+            var worksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+            worksheetPart.Worksheet = new(new SheetData());
 
-        var sheetData = worksheetPart.Worksheet.GetFirstChild<SheetData>()!;
+            var sheets = workbookPart.Workbook.GetFirstChild<Sheets>()!;
+            sheets.Append(new Sheet
+            {
+                Id = workbookPart.GetIdOfPart(worksheetPart),
+                SheetId = 1,
+                Name = "Sheet1"
+            });
 
-        var headerRow = new Row { RowIndex = 1 };
-        headerRow.Append(CreateCell("A1", "Name"));
-        headerRow.Append(CreateCell("B1", "Value"));
-        sheetData.Append(headerRow);
+            var stylesPart = workbookPart.AddNewPart<WorkbookStylesPart>();
+            stylesPart.Stylesheet = new(
+                new Fonts(new Font()),
+                new Fills(
+                    new Fill(new PatternFill { PatternType = PatternValues.None }),
+                    new Fill(new PatternFill { PatternType = PatternValues.Gray125 })),
+                new Borders(new Border()),
+                new CellFormats(new CellFormat()));
 
-        var dataRow = new Row { RowIndex = 2 };
-        dataRow.Append(CreateCell("A2", "Test"));
-        dataRow.Append(CreateCell("B2", "123"));
-        sheetData.Append(dataRow);
+            var sheetData = worksheetPart.Worksheet.GetFirstChild<SheetData>()!;
 
-        var outputStream = new MemoryStream();
-        document.Clone(outputStream);
-        outputStream.Position = 0;
-        return outputStream;
+            var headerRow = new Row { RowIndex = 1 };
+            headerRow.Append(CreateCell("A1", "Name"));
+            headerRow.Append(CreateCell("B1", "Value"));
+            sheetData.Append(headerRow);
+
+            var dataRow = new Row { RowIndex = 2 };
+            dataRow.Append(CreateCell("A2", "Test"));
+            dataRow.Append(CreateCell("B2", "123"));
+            sheetData.Append(dataRow);
+        }
+
+        stream.Position = 0;
+        return stream;
     }
 
     static Cell CreateCell(string reference, string value) =>
